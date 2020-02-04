e1572b45b91901fa43075b3c0ba36686173a3a8c
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-02-04 19:40:03 +0000

    fizzbuzz

--- clojura/src/clojura/core.clj
+++ clojura/src/clojura/core.clj
@@ -2,7 +2,16 @@
   (:gen-class)
   (:require [clojura.math :refer :all]))
 
+(defn fizzbuzz [start finish]
+  (map (fn [n]
+         (cond
+           (zero? (mod n 15)) "FizzBuzz"
+           (zero? (mod n 3)) "Fizz"
+           (zero? (mod n 5)) "Buzz"
+           :else n))
+       (range start finish)))
+
 (defn -main [& args]
-  (println "hello clojura")
+  (println (fizzbuzz 1 55))
   (println (factorial 5))
   (println (factorial-bigint 21)))
