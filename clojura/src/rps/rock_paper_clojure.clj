6d093113f3f73dae8e7e0776cdc749992d12c9c4
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-02-10 20:24:23 +0000

    winner function

--- clojura/src/rps/rock_paper_clojure.clj
+++ clojura/src/rps/rock_paper_clojure.clj
@@ -6,5 +6,16 @@
   (let [guess (read-line)]
     (if (get {"r" true "p" true "s" true} guess) guess)))
 
+(defn winner [guess1 guess2]
+  (let [guesses [guess1 guess2]]
+    (cond
+      (= guess1 guess2) 0
+      (= guesses ["p" "r"]) 1
+      (= guesses ["r" "p"]) 2
+      (= guesses ["r" "s"]) 1
+      (= guesses ["s" "r"]) 2
+      (= guesses ["s" "p"]) 1
+      (= guesses ["p" "s"]) 2)))
+
 (defn -main [& args]
   (println (get-guess)))
