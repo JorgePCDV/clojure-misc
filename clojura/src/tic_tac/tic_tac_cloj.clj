0878146ccf60a03a44fe30ede963064b45d93a82
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-02-17 22:18:48 +0000

    winner? function

--- clojura/src/tic_tac/tic_tac_cloj.clj
+++ clojura/src/tic_tac/tic_tac_cloj.clj
@@ -5,4 +5,9 @@
   (if (every? #{:x} triple) :x
     (if (every? #{:o} triple) :o)))
 
+(declare triples)
+
+(defn winner? [board]
+  (first (filter #{:x :o} (map triple-winner? (triples board)))))
+
 (defn -main [& args])
