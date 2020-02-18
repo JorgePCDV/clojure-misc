bd11bdbbc3b04caa70072d3c67262610d9bd48a8
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-02-18 18:55:49 +0000

    triples function initial

--- clojura/src/tic_tac/tic_tac_cloj.clj
+++ clojura/src/tic_tac/tic_tac_cloj.clj
@@ -10,4 +10,12 @@
 (defn winner? [board]
   (first (filter #{:x :o} (map triple-winner? (triples board)))))
 
+(defn triples [board]
+  (concat
+    (partition-all 3 board)
+    (list
+        (take-nth 3 board)
+        (take-nth 3 (drop 1 board))
+        (take-nth 3 (drop 2 board)))))
+
 (defn -main [& args])
