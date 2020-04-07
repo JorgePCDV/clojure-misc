d32ca237efd1375a86a9bd33fa5a3f835926ae5e
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-04-07 18:05:13 +0100

    error message function xample

--- cloj-bratrve/src/cloj_bratrve/core.clj
+++ cloj-bratrve/src/cloj_bratrve/core.clj
@@ -6,6 +6,13 @@
   [x]
   (println "Hello, " x))
 
+(defn error-message
+  [severity]
+  (str "Displaying error message: "
+       (if (= severity :mild)
+         "mild error"
+         "alert")))
+
 (defn -main [& args]
   (foo "clojure"))
 
