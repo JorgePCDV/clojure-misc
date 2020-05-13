64397009eb40fe892684f25cb8760da80f383f8b
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-05-13 18:30:27 +0100

    take-while, drop-while

--- cloj-bratrve/src/cloj_bratrve/core.clj
+++ cloj-bratrve/src/cloj_bratrve/core.clj
@@ -53,6 +53,21 @@
   [numbers]
   (map #(% numbers) [sum count avg]))
 
+(def food-journal
+  [{:month 1 :day 1 :pizza 5.3 :pasta 2.3}
+   {:month 1 :day 2 :pizza 5.1 :pasta 2.0}
+   {:month 2 :day 1 :pizza 4.9 :pasta 2.1}
+   {:month 2 :day 2 :pizza 5.0 :pasta 2.5}
+   {:month 3 :day 1 :pizza 4.2 :pasta 3.3}
+   {:month 3 :day 2 :pizza 4.0 :pasta 3.8}
+   {:month 4 :day 1 :pizza 3.7 :pasta 3.9}
+   {:month 4 :day 2 :pizza 3.7 :pasta 3.6}])
+
+(take-while #(< (:month %) 3) food-journal)
+(drop-while #(< (:month %) 3) food-journal)
+(take-while #(< (:month %) 4)
+  (drop-while #(< (:month %) 2) food-journal))
+
 (defn append-function
   [arg]
   (str arg " appended"))
