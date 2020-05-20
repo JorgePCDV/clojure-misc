2a8a0bf5d847c351ab45b176ba50486970b2ff1e
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-05-20 20:53:52 +0100

    even numbers lazy seq recursion example

--- cloj-bratrve/src/cloj_bratrve/core.clj
+++ cloj-bratrve/src/cloj_bratrve/core.clj
@@ -73,6 +73,11 @@
   [arg]
   (str arg " appended"))
 
+(defn even-numbers
+  ([] (even-numbers 0))
+  ([n] (cons n (lazy-seq (even-numbers (+ n 2))))))
+(take 10 (even-numbers))
+
 (concat [1 2] '(3 4))
 
 (defn -main [& args]
