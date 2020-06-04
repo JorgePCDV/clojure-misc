1d6d6f9e87a4d432e442ec6544b2d1c93359aac7
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-06-04 17:24:55 +0100

    fwpd initial changes

--- fwpd/src/fwpd/core.clj
+++ fwpd/src/fwpd/core.clj
@@ -1,5 +1,18 @@
 (ns fwpd.core)
+
 (def filename "suspects.csv")
+
+(def vamp-keys [:name :glit-index])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity :glit-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
 
 (defn -main [& args]
   (println (slurp filename)))
