c92020cf03670e2284257bd6bdb04b24dd433274
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-07-08 19:31:18 +0100

    change project clj require set

--- peg/src/peg/core.clj
+++ peg/src/peg/core.clj
@@ -1,6 +1,12 @@
-(ns peg.core)
+(ns peg.core
+  (require [clojure.set :as set])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn prompt-rows
+  []
+  (println "How many rows? [5]"))
+
+(defn -main
+  [& args]
+  (println "Get ready to play!")
+  (prompt-rows))
