74fdbf7b9d7a68cff573a7c65ef5646f74d5ac56
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-07-09 18:19:55 +0100

    prompt-rows method

--- peg/src/peg/core.clj
+++ peg/src/peg/core.clj
@@ -1,10 +1,22 @@
 (ns peg.core
-  (require [clojure.set :as set])
+  (:require [clojure.set :as set])
   (:gen-class))
+
+(defn get-input
+  "Waits for user to enter text and hit enter, then cleans input"
+  ([] (get-input ""))
+  ([default]
+   (let [input (clojure.string/trim (read-line))]
+     (if (empty? input)
+       default
+       (clojure.string/lower-case input)))))
 
 (defn prompt-rows
   []
-  (println "How many rows? [5]"))
+  (println "How many rows? [5]")
+  (let [rows (Integer. (get-input 5))
+        board (new-board rows)]
+    (prompt-empty-peg board)))
 
 (defn -main
   [& args]
