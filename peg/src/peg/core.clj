48853a4656baa88e58ba4a9e09667044babc32fe
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-07-15 18:16:37 +0100

    connector functions

--- peg/src/peg/core.clj
+++ peg/src/peg/core.clj
@@ -2,6 +2,28 @@
   (:require [clojure.set :as set])
   (:gen-class))
 
+(defn connect-right
+  [board max-pos pos]
+  (let [neighbor (inc pos)
+        destination (inc neighbor)]
+    (if-not (or (triangular? neighbor) (triangular? pos))
+      (connect board max-pos pos neighbor destination)
+      board)))
+
+(defn connect-down-left
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbor (+ row pos)
+        destination (+ 1 row neighbor)]
+    (connect board max-pos pos neighbor destination)))
+
+(defn connect-down-right
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbor (+ 1 row pos)
+        destination (+ 2 row neighbor)]
+    (connect board max-pos pos neighbor destination)))
+
 (defn add-pos
   "Pegs the position and performs connections"
   [board max-pos pos]
