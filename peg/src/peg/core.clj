b3bc3f7f38934e06bc6b3d084d7f6c36b0a195ef
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-09-07 18:01:56 +0100

    working version of peg

--- peg/src/peg/core.clj
+++ peg/src/peg/core.clj
@@ -1,64 +1,15 @@
 (ns peg.core
-  (:require [clojure.set :as set])
+  (require [clojure.set :as set])
   (:gen-class))
 
 (declare successful-move prompt-move game-over prompt-rows)
 
 ;;;;
-;; Represent board textually and print it
-;;;
-(def alpha-start 97)
-(def alpha-end 123)
-(def letters (map (comp str char) (range alpha-start alpha-end)))
-(def pos-chars 3)
-
-(defn ansi-styles
-  {:red "[31m"
-   :green "[32m"
-   :blue "34m"
-   :reset "[0m"})
-
-(defn ansi
-  "Produce a string which will apply an ansi style"
-  [style]
-  (str \u001b (style ansi-styles)))
-
-(defn colorize
-  "Apply ansi color to text"
-  [text color]
-  (str (ansi color) text (ansi :reset)))
-
-(defn render-pos
-  [board pos]
-  (str (nth letters (dec pos))
-       (if (get-in board [pos :pegged])
-         (colorize "0" :blue)
-         (colorize "-" :red))))
-
-(defn row-positions
-  "Return all positions in the given row"
-  [row-num]
-  (range (inc (or (row-tri (dec row-num)) 0))
-         (inc (row-tri row-num))))
-
-(defn row-padding
-  "String of spaces to add to the beginning of a row to center it"
-  [row-num rows]
-  (let [pad-length (/ (* (- rows row-num) pos-chars) 2)]
-    (apply str (take pad-length (repeat " ")))))
-
-(defn render-row [board row-num]
-  (str (row-padding row-num (:rows board)))
-        (clojure.string/join " " (map (partial render-pos board) (row-positions row-num))))
-
-(defn print-board
-  [board]
-  (doseq [row-num (range 1 (inc (:rows board)))]
-    (println (render-row board row-num))))
-
+;; Create the board
+;;;;
 (defn tri*
   "Generates lazy sequence of triangular numbers"
-  ([] tri* 0 1)
+  ([] (tri* 0 1))
   ([sum n]
    (let [new-sum (+ sum n)]
      (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
@@ -66,7 +17,7 @@
 (def tri (tri*))
 
 (defn triangular?
-  "Is the number triangular? e.g. 1, 3, 6, 10, 15, etc."
+  "Is the number triangular? e.g. 1, 3, 6, 10, 15, etc"
   [n]
   (= n (last (take-while #(>= n %) tri))))
 
@@ -77,7 +28,7 @@
 
 (defn row-num
   "Returns row number the position belongs to: pos 1 in row 1,
-   positions 2 and 3 in row 2, etc."
+  positions 2 and 3 in row 2, etc"
   [pos]
   (inc (count (take-while #(> pos %) tri))))
 
@@ -92,7 +43,8 @@
   (if (in-bounds? max-pos neighbor destination)
     (reduce (fn [new-board [p1 p2]] (assoc-in new-board [p1 :connections p2] neighbor))
             board
-            [[pos destination] [destination pos]])))
+            [[pos destination] [destination pos]])
+    board))
 
 (defn connect-right
   [board max-pos pos]
@@ -131,6 +83,109 @@
     (reduce (fn [board pos] (add-pos board max-pos pos))
             initial-board
             (range 1 (inc max-pos)))))
+;;;;
+;; Move pegs
+;;;;
+(defn pegged?
+  "Does the position have a peg in it?"
+  [board pos]
+  (get-in board [pos :pegged]))
+
+(defn valid-moves
+  "Return a map of all valid moves for pos, where the key is the
+  destination and the value is the jumped position"
+  [board pos]
+  (into {}
+        (filter (fn [[destination jumped]]
+                  (and (not (pegged? board destination))
+                       (pegged? board jumped)))
+                (get-in board [pos :connections]))))
+
+(defn valid-move?
+  "Return jumped position if the move from p1 to p2 is valid, nil
+  otherwise"
+  [board p1 p2]
+  (get (valid-moves board p1) p2))
+
+(defn remove-peg
+  "Take the peg at given position out of the board"
+  [board pos]
+  (assoc-in board [pos :pegged] false))
+
+(defn place-peg
+  "Put a peg in the board at given position"
+  [board pos]
+  (assoc-in board [pos :pegged] true))
+
+(defn move-peg
+  "Take peg out of p1 and place it in p2"
+  [board p1 p2]
+  (place-peg (remove-peg board p1) p2))
+
+(defn make-move
+  "Move peg from p1 to p2, removing jumped peg"
+  [board p1 p2]
+  (if-let [jumped (valid-move? board p1 p2)]
+    (move-peg (remove-peg board jumped) p1 p2)))
+
+(defn can-move?
+  "Do any of the pegged positions have valid moves?"
+  [board]
+  (some (comp not-empty (partial valid-moves board))
+        (map first (filter #(get (second %) :pegged) board))))
+
+;;;;
+;; Represent board textually and print it
+;;;;
+(def alpha-start 97)
+(def alpha-end 123)
+(def letters (map (comp str char) (range alpha-start alpha-end)))
+(def pos-chars 3)
+
+(def ansi-styles
+  {:red   "[31m"
+   :green "[32m"
+   :blue  "[34m"
+   :reset "[0m"})
+
+(defn ansi
+  "Produce a string which will apply an ansi style"
+  [style]
+  (str \u001b (style ansi-styles)))
+
+(defn colorize
+  "Apply ansi color to text"
+  [text color]
+  (str (ansi color) text (ansi :reset)))
+
+(defn render-pos
+  [board pos]
+  (str (nth letters (dec pos))
+       (if (get-in board [pos :pegged])
+         (colorize "0" :blue)
+         (colorize "-" :red))))
+
+(defn row-positions
+  "Return all positions in the given row"
+  [row-num]
+  (range (inc (or (row-tri (dec row-num)) 0))
+         (inc (row-tri row-num))))
+
+(defn row-padding
+  "String of spaces to add to the beginning of a row to center it"
+  [row-num rows]
+  (let [pad-length (/ (* (- rows row-num) pos-chars) 2)]
+    (apply str (take pad-length (repeat " ")))))
+
+(defn render-row
+  [board row-num]
+  (str (row-padding row-num (:rows board))
+       (clojure.string/join " " (map (partial render-pos board) (row-positions row-num)))))
+
+(defn print-board
+  [board]
+  (doseq [row-num (range 1 (inc (:rows board)))]
+    (println (render-row board row-num))))
 
 ;;;;
 ;; Interaction
@@ -141,7 +196,7 @@
   (inc (- (int (first letter)) alpha-start)))
 
 (defn get-input
-  "Waits for user to enter text and hit enter, then cleans input"
+  "Waits for user to enter text and hit enter, then cleans the input"
   ([] (get-input ""))
   ([default]
    (let [input (clojure.string/trim (read-line))]
@@ -149,41 +204,11 @@
        default
        (clojure.string/lower-case input)))))
 
-;;;;
-;; Move pegs
-;;;;
-(defn pegged?
-  "Does the position have a peg in it?"
-  [board pos]
-  (get-in board [pos :pegged]))
-
-(defn valid-moves
-  "Return a map of all valid moves for pos, 
-  where the key is the destination and the value is the jumped position"
-  [board pos]
-  (into {}
-        (filter (fn [[destination jumped]]
-                  (and (not (pegged? board destination))
-                       (pegged? board jumped)))
-                (get-in board [pos :connections]))))
-
-(defn valid-move?
-  "Return jumped position if the move from p1 to p2 is valid,
-  nil otherwise"
-  [board p1 p2]
-  (get (valid-moves board p1) p2))
-
-(defn can-move?
-  "Do any of the pegged positions have valid moves?"
-  [board]
-  (some (comp not-empty (partial valid-moves board))
-        (map first (filter #(get (second %) :pegged) board))))
-
-(defn successful-move
-  [board]
-  (if (can-move? board)
-    (prompt-move board)
-    (game-over board)))
+(defn characters-as-strings
+  "Given a string, return a collection consisting of each individual
+  character"
+  [string]
+  (re-seq #"[a-zA-Z]" string))
 
 (defn prompt-move
   [board]
@@ -194,13 +219,27 @@
     (if-let [new-board (make-move board (first input) (second input))]
       (successful-move new-board)
       (do
-        (println "\n That was an invalid move!")
+        (println "\n!!! That was an invalid move :(\n")
         (prompt-move board)))))
 
-(defn remove-peg
-  "Take the peg at given position out of the board"
-  [board pos]
-  (assoc-in board [pos :pegged] false))
+(defn successful-move
+  [board]
+  (if (can-move? board)
+    (prompt-move board)
+    (game-over board)))
+
+(defn game-over
+  [board]
+  (let [remaining-pegs (count (filter :pegged (vals board)))]
+    (println "Game over! You had" remaining-pegs "pegs left:")
+    (print-board board)
+    (println "Play again? y/n [y]")
+    (let [input (get-input "y")]
+      (if (= "y" input)
+        (prompt-rows)
+        (do
+          (println "Bye!")
+          (System/exit 0))))))
 
 (defn prompt-empty-peg
   [board]
@@ -218,5 +257,5 @@
 
 (defn -main
   [& args]
-  (println "Get ready to play!")
+  (println "Get ready to play peg thing!")
   (prompt-rows))
