6298edac11dba92517e5b9130a00848f10b54fcc
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-10-07 18:55:48 +0100

    functions in svg.

--- namespaces-sample/src/namespaces_sample/visualization/svg.clj
+++ namespaces-sample/src/namespaces_sample/visualization/svg.clj
@@ -1,5 +1,14 @@
 (ns namespaces-sample.visualization.svg)
 
+(defn latlng->point
+  "Convert lat/lng map to comma-separated string"
+  [latlng]
+  (str (:lat latlng) "," (:lng latlng)))
+
+(defn points
+  [locations]
+  (clojure.string/join " " (map latlng->point locations)))
+
 (defn foo
   "I don't do a whole lot."
   [x]
