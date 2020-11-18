4d0cb4baf3440e6ce51031bf6811450d3f126bec
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-11-18 15:04:35 +0000

    comparator-over-maps.

--- namespaces-sample/src/namespaces_sample/visualization/svg.clj
+++ namespaces-sample/src/namespaces_sample/visualization/svg.clj
@@ -1,4 +1,16 @@
-(ns namespaces-sample.visualization.svg)
+(ns namespaces-sample.visualization.svg
+  (:require [clojure.string :as s])
+  (:refer-clojure :exclude [min max]))
+
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks
+            (map (fn [k] (apply comparison-fn (map k maps)))
+                 ks))))
+
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
 
 (defn latlng->point
   "Convert lat/lng map to comma-separated string "
