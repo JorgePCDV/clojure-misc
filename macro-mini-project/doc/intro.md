6c7a07afbeea00a88642495c7c6eb0109d72266c
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-03-14 11:57:26 +0000

    macro project structure.

---
+++ macro-mini-project/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to macro-mini-project
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ macro-mini-project/test/macro_mini_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns macro-mini-project.core-test
+  (:require [clojure.test :refer :all]
+            [macro-mini-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
