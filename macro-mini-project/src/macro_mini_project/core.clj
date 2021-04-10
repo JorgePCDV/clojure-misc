8fddb95848dafc0427b5dae06ea2f1e297f449a3
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-04-10 20:05:52 +0100

    macro-mini-project order-details-validations

--- macro-mini-project/src/macro_mini_project/core.clj
+++ macro-mini-project/src/macro_mini_project/core.clj
@@ -1,8 +1,17 @@
 (ns macro-mini-project.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def order-details
+  {:name "Some Name"
+   :email "some.nameemail.com"})
+
+(def order-details-validations
+  {:name
+   ["Please enter a name" not-empty]
+
+   :email
+   ["Please enter an email address" not-empty
+
+    "Your email address doesn't look like an email address"
+    #(or (empty? %) (re-seq #"@" %))]})
 
 
