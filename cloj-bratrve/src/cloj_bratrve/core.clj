6b19d8d0b5009890d1000b0dc506521c36de9319
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-07-31 19:33:33 +0100

    pmap example.

--- cloj-bratrve/src/cloj_bratrve/core.clj
+++ cloj-bratrve/src/cloj_bratrve/core.clj
@@ -458,5 +458,17 @@
     .start
     .join))
 
+;; pmap example
+(def alphabet-length 26)
+(def letters (mapv (comp str char (partial + 65)) (range alphabet-length)))
+(defn random-string
+  "Returns a random string of specified length"
+  [length]
+  (apply str (take length (repeatedly #(rand-nth letters)))))
+(defn random-string-list
+  [list-length string-length]
+  (doall (take list-length (repeatedly (partial random-string string-length)))))
+(def names (random-string-list 3000 7000))
+
   (defn -main [& args]
     (foo "clojure"))
