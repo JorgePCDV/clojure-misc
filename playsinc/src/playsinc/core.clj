be8e08ae985d3177079ef54f86bd26b37819db2b
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-08-21 16:38:19 +0100

    process pipeline example.

--- playsinc/src/playsinc/core.clj
+++ playsinc/src/playsinc/core.clj
@@ -63,3 +63,12 @@
 
   (>!! in 3)
   (<!! out))
+
+;; process pipeline
+(let [c1 (chan)
+      c2 (chan)
+      c3 (chan)]
+  (go (>! c2 (clojure.string/upper-case (<! c1))))
+  (go (>! c3 (clojure.string/reverse (<! c2))))
+  (go (println (<! c3)))
+  (>!! c1 "hello"))
