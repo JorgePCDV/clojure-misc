6a3240da2a7c6c1eae9da5fef5da2b033e52d114
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-10-06 16:10:57 +0100

    process pipelines

--- playsinc/src/playsinc/core.clj
+++ playsinc/src/playsinc/core.clj
@@ -125,3 +125,26 @@
   (let [c (chan)]
     (go (while true (append-to-file filename (<! c))))
     (dotimes [n num-quotes] (go (>! c (random-quote))))))
+
+;; process pipelines
+(defn upper-caser
+  [in]
+  (let [out (chan)]
+    (go (while true (>! out (clojure.string/upper-case (<! in)))))
+    out))
+
+(defn reverser
+  [in]
+  (let [out (chan)]
+    (go (while true (>! out (clojure.string/reverse (<! in)))))
+    out))
+
+(defn printer
+  [in]
+  (go (while true (println (<! in)))))
+
+(def in-chan (chan))
+(def upper-caser-out (upper-caser in-chan))
+(def reverser-out (reverser upper-caser-out))
+(printer reverser-out)
+(>!! in-chan "hello")
